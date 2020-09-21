fb4a07eef436fc457ba0c77f322d75ae5d8b3dcb
wlgud0402 <wlgudrlgus@naver.com>
2020-09-21 19:19:57 +0900

    bookmark app

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from bookmark.models import Bookmark
 
 # Register your models here.
+
+
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'url')
