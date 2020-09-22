494bb4a4c3edd8163a057888cfea3d65c9b993da
wlgud0402 <wlgudrlgus@naver.com>
2020-09-22 20:49:38 +0900

    템플릿 완성

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,27 @@
-from django.urls import path
+from django.urls import path, re_path
 from blog import views
 
-app_name = 'blog'
+app_name='blog'
 urlpatterns = [
+    # /blog/ == /blog/post/
+    path('', views.PostLV.as_view(), name='index'), 
+    path('post/', views.PostLV.as_view(), name='post_list'),
 
+    #/blog/post/django-example/
+    re_path(r'^post/(?P<slug>[-\w]+)/$', views.PostDV.as_view(), name='post_detail'),
+
+    #/blog/archive/
+    path('archive/', views.PostAV.as_view(), name='post_archive'),
+
+    #/blog/archive/2019/
+    path('archive/<int:year>/', views.PostYAV.as_view(), name='post_year_archive'),
+
+    #/blog/archive/2019/nov/
+    path('archive/<int:year>/<str:month>/', views.PostMAV.as_view(), name='post_month_archive'),
+
+    #/blog/archive/2019/nov/10/
+    path('archive/<int:year>/<str:month>/<int:day>/', views.PostDAV.as_view(), name='post_day_archive'),
+
+    #/blog/archive/today/
+    path('archive/today/', views.PostTAV.as_view(), name='post_today_archive'),
 ]
